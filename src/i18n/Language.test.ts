f9ae0de32b538038f380d9465462dd9778ebd54a
Liis S & Madli <madli.petuhhov89@gmail.com>
2024-11-14 10:36:58 +0200

    simplified code and tests

--- src/i18n/Language.test.ts
+++ src/i18n/Language.test.ts
@@ -1,132 +1,54 @@
-import {beforeEach, describe, expect, it, vi} from "vitest"
-import {
-    getLangCode,
-    getLangFromNavigator,
-    getLangFromSession,
-    loadTranslation,
-    changeLang, toLang
-} from "./Language"
+import {afterEach, describe, expect, it, vi} from "vitest"
+import en from './en.json'
+import et from './et.json'
+import {changeLang, detectLang, loadTranslation} from "./Language"
 import type {Lang} from "../utils/Types"
 
-beforeEach(() => {
-    vi.clearAllMocks()
+afterEach(() => {
+    sessionStorage.clear()
 })
 
-describe('getLangFromSession', () => {
-    it('should return the language from sessionStorage if available and in langs available', () => {
-        sessionStorage.setItem('selectedLang', 'en')
-        const result = getLangFromSession()
-        expect(result).toBe('en')
-    })
-
-    it('should return null if no language is set in sessionStorage', () => {
-        sessionStorage.setItem('selectedLang', '')
-        const result = getLangFromSession()
-        expect(result).toBeNull()
-    })
-
-    it('should return null if language not available in langs', () => {
-        sessionStorage.setItem('selectedLang', 'fr')
-        const result = getLangFromSession()
-        expect(result).toBeNull()
-    })
-})
-
-describe('getLangFromNavigator', () => {
-    it('should return the language code from navigator.language and in langs available', () => {
-        vi.spyOn(navigator, 'language', 'get').mockReturnValue('en-US')
-        const result = getLangFromNavigator()
-        expect(result).toBe('en')
-    })
-
-    it('should return null if no language is set in navigator.language', () => {
-        vi.spyOn(navigator, 'language', 'get').mockReturnValue('')
-        const result = getLangFromNavigator()
-        expect(result).toBeNull()
-    })
-
-    it('should return null if language not available in langs', () => {
-        vi.spyOn(navigator, 'language', 'get').mockReturnValue('fr')
-        const result = getLangFromNavigator()
-        expect(result).toBeNull()
-    })
-})
-
-describe('to Lang', () => {
-    it('should return null if lang is undefined', () => {
-        const result = toLang()
-        expect(result).toBeNull()
-    })
-
-    it('should return null if lang is an empty string', () => {
-        const result = toLang('')
-        expect(result).toBeNull()
-    })
-
-    it('should return null if lang is not in the langs array', () => {
-        const result = toLang('fr')
-        expect(result).toBeNull()
-    })
-
-    it('should return the lang if it is a valid language code', () => {
-        const result = toLang('et')
-        expect(result).toBe('et')
-    })
-})
-
-describe('getLangCode', () => {
+describe('detectLang', () => {
     it('should return the language from sessionStorage if available', () => {
         sessionStorage.setItem('selectedLang', 'et')
-        const result = getLangCode()
+        const result = detectLang()
         expect(result).toBe('et')
     });
 
     it('should return the language from navigator.language if sessionStorage language is not available', () => {
-        sessionStorage.removeItem('selectedLang')
         vi.spyOn(navigator, 'language', 'get').mockReturnValue('et')
-        const result = getLangCode()
+        const result = detectLang()
         expect(result).toBe('et')
     })
 
     it('should return the fallback language if sessionStorage and navigator.language are not valid', () => {
-        sessionStorage.removeItem('selectedLang')
         vi.spyOn(navigator, 'language', 'get').mockReturnValue('fr')
-        const result = getLangCode()
+        const result = detectLang()
         expect(result).toBe('en')
     })
 })
 
 describe('loadTranslation', () => {
     it('should load the correct translation file for the selected language', async () => {
-        const mockLangCode = 'et'
-        const mockEstonianTranslation = {title: 'Elektrihinnad'}
-        vi.mock('../et.json', () => Promise.resolve(mockEstonianTranslation))
-
-        const result = await loadTranslation(mockLangCode)
-
-        expect(result.title).toEqual(mockEstonianTranslation.title)
+        const result = await loadTranslation('et')
+        expect(result.title).toEqual(et.title)
     })
 
     it('should log an error and fall back to the default translation file if import fails', async () => {
-        const languageCode = 'fr'
-        const mockFallbackTranslation = {title: 'Electricity prices'}
-        vi.spyOn(console, 'error').mockImplementation(() => {})
-        vi.mock('../en.json', () => Promise.resolve(mockFallbackTranslation))
+        vi.spyOn(console, 'error')
 
-        const result = await loadTranslation(languageCode)
+        const result = await loadTranslation('fr')
 
-        expect(result.title).toEqual(mockFallbackTranslation.title)
-        expect(console.error).toHaveBeenCalledWith(`Failed to load translation for language: ${languageCode}`)
+        expect(result).to.deep.equal(en)
+        expect(console.error).toHaveBeenCalledWith(`Failed to load translation for language: fr`)
     })
 })
 
 describe('changeLang', () => {
     it('should set the correct langCode in sessionStorage and reload the page', () => {
-        sessionStorage.clear()
         vi.stubGlobal('location', {reload: vi.fn()})
-        const langCode: Lang = 'en'
 
-        changeLang(langCode)
+        changeLang('en')
 
         expect(sessionStorage.getItem('selectedLang')).toEqual('en')
         expect(location.reload).toHaveBeenCalled()
--- src/i18n/Language.ts
+++ src/i18n/Language.ts
@@ -2,40 +2,34 @@ import type {Lang} from '../utils/Types'
 import type et from './et.json'
 import langs from './langs.json'
 
-export let langCode: Lang = getLangCode()
+export let langCode: Lang = detectLang()
 export let t: typeof et = await loadTranslation(langCode)
 
-export function getLangCode(): Lang {
-    const langFromSession = getLangFromSession()
-    if (langFromSession) return langFromSession
-
-    const langFromNavigator = getLangFromNavigator()
-    if (langFromNavigator) return langFromNavigator
-
-    return langs[0]
+export function detectLang(): Lang {
+    return getLangFromSession() ?? getLangFromNavigator() ?? langs[0]
 }
 
-export function getLangFromSession(): Lang | null {
-    let langFromSession = sessionStorage.getItem('selectedLang')?.trim()
-    return toLang(langFromSession)
+function getLangFromSession(): Lang | undefined {
+    const langFromSession = sessionStorage['selectedLang']
+    return ensureSupportedLang(langFromSession)
 }
 
-export function getLangFromNavigator(): Lang | null {
-    let langFromNavigator = navigator.language.split('-')[0]
-    return toLang(langFromNavigator)
+function getLangFromNavigator(): Lang | undefined {
+    const langFromNavigator = navigator.language.split('-')[0]
+    return ensureSupportedLang(langFromNavigator)
 }
 
-export function toLang(lang?: string): Lang | null {
-    if (!lang || !langs.includes(lang)) return null
-    return lang as Lang;
+function ensureSupportedLang(lang?: string): Lang | undefined {
+    if (!lang || !langs.includes(lang)) return
+    return lang as Lang
 }
 
 export async function loadTranslation(langCode: Lang) {
     try {
-        return await import(`./${langCode}.json`)
+        return (await import(`./${langCode}.json`)).default
     } catch (error) {
         console.error(`Failed to load translation for language: ${langCode}`)
-        return await import("./en.json")
+        return (await import('./en.json')).default
     }
 }
 
